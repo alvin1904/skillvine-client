32515d4671ae990523343e9614e94020859f9d51
Alvin <alvin19official@gmail.com>
2023-05-02 22:27:38 +0530

    feat: sidebar scores onClick update

--- src/layouts/user/Sidebar.js
+++ src/layouts/user/Sidebar.js
@@ -7,39 +7,39 @@ import { getScoreAPI } from "@/apis";
 export default function Sidebar() {
   const { loading, fetchData } = useAxiosCaller();
   const { throwError } = useCustomError();
-  const [seedCheck, setSeedCheck] = useState(true);
-
   const [targetScore, setTargetScore] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
 
-  useEffect(() => {
-    setSeedCheck(true);
-  }, []);
-  useEffect(() => {
-    const getScores = async () => {
-      setSeedCheck(false);
-      const response = await fetchData(getScoreAPI);
-      if ([200, 304].includes(response?.status)) {
-        setTargetScore(response?.data?.targetScore);
-        setCurrentScore(response?.data?.currentScore);
-      } else if (response?.response?.status === 401) console.log("Token not present");
-      else throwError(response?.response?.status);
-    };
+  const getScores = async () => {
+    const response = await fetchData(getScoreAPI);
+    if ([200, 304].includes(response?.status)) {
+      setTargetScore(response?.data?.targetScore);
+      setCurrentScore(response?.data?.currentScore);
+    } else if (response?.response?.status === 401)
+      throwError("Please login & try again!");
+    else throwError(response?.response?.status);
+  };
 
-    seedCheck && getScores();
-  }, [seedCheck]);
+  useEffect(() => {
+    getScores();
+  }, []);
+
+  const handleUpdate = async () => {
+    if (targetScore !== currentScore || targetScore !== 0) return;
+    await getScores();
+  };
 
   return (
     <aside className={styles.sidebar}>
-      <div className={styles.sidebar_section}>
+      <div className={styles.sidebar_section} onClick={handleUpdate}>
         <p>No of activity points:</p>
         <span>{currentScore || 0}</span>
       </div>
-      <div className={styles.sidebar_section}>
+      <div className={styles.sidebar_section} onClick={handleUpdate}>
         <p>Total no. of of activity points required:</p>
         <span>{targetScore || 0}</span>
       </div>
-      <div className={styles.sidebar_section}>
+      <div className={styles.sidebar_section} onClick={handleUpdate}>
         <p>No of activity points needed: </p>
         <span>{targetScore - currentScore || 0}</span>
       </div>
--- src/pages/student/certificates/add/index.js
+++ src/pages/student/certificates/add/index.js
@@ -2,6 +2,7 @@
 
 import AddCertificate from "@/componentsUser/certificate/AddCertificate";
 import StudentLayout from "@/layouts/StudentLayout";
+import Head from "next/head";
 import React from "react";
 
 export default function addCertificates() {
