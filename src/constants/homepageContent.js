09a9b2ce8bd04b0d16e1e085f6121f6d210ee70e
alvin <alvin19official@gmail.com>
2023-06-18 19:27:17 +0530

    feat: add images to homepage

--- src/constants/homepageContent.js
+++ src/constants/homepageContent.js
@@ -1,21 +1,21 @@
 const homePageContent = [
   {
-    imageLink: "/assets/sample_image.jpg",
+    imageLink: "/assets/Collage1.png",
     content:
       "Our platform is designed to simplify the process of managing students' certificates and tracking their points. With our user-friendly interface and powerful features, teachers and administrators can efficiently handle certificate evaluations and  points allocation and ensure compliance with university guidelines. We enable students to access their real time points and notification on teacher markings.",
   },
   {
-    imageLink: "/assets/sample_image.jpg",
+    imageLink: "/assets/Collage2.png",
     content:
       "With Skill Vine, students can log in to their personalized dashboard and view their current certificate points . Our intuitive interface provides  features to add and edit their certificates ,a clear overview of the points required to meet the university guidelines,profile details,notification system and so on.In the case of certificate  rejection, students are provided with clear remark form the faculty.Students can access the web with their institution mail from anywhere and navigate to any of these feature easily from their home dashboard.We also provide students the option, if necessary, to remove their accounts. ",
   },
   {
-    imageLink: "/assets/sample_image.jpg",
+    imageLink: "/assets/Collage3.png",
     content:
       "On to the teachers side our platform offers a specially tailored dashboard , providing them with powerful tools to streamline the certificate evaluation process and provide  feedback to students.Mark suggestions,review and remark ,certificate acceptance and rejection,profile deletion etc, are main features present in the teachers side. Also teachers can generate batch-wise reports, providing them with a comprehensive overview of students activity point  statistics for each batch.",
   },
   {
-    imageLink: "/assets/sample_image.jpg",
+    imageLink: "/assets/Collage4.png",
     content:
       "We aim to provide an easy,seamless and transparent solution where students and teachers can depend on for activity points and certificate management without any fail",
   },
